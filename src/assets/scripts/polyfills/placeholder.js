c174a14a4932ad1d320ee0e5eaabf56bb699de10
Tomohiro Kasuga <tk@byodkm.com>
2015-05-09 09:29:09 +0900

    Add placeholder polyfill

---
+++ src/assets/scripts/polyfills/placeholder.js
@@ -0,0 +1,48 @@
+(function() {
+  var blurred, focused, hasOwnAbility, loaded;
+
+  hasOwnAbility = function() {
+    var input;
+    input = document.createElement('input');
+    if ('placeholder' in input) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  if (hasOwnAbility()) {
+    return;
+  }
+
+  focused = function() {
+    this.setAttribute('data-placeholder', 'false');
+    if (this.value === this.placeholder) {
+      this.value = '';
+    }
+  };
+
+  blurred = function() {
+    if (this.value === '') {
+      this.value = this.placeholder;
+      this.setAttribute('data-placeholder', 'true');
+    }
+  };
+
+  loaded = function() {
+    var i, node, nodes;
+    nodes = document.querySelectorAll('[placeholder]');
+    for (i = nodes.length - 1; i >= 0; i += -1) {
+      node = nodes[i];
+      if (node.value === '') {
+        node.value = node.placeholder;
+        node.setAttribute('data-placeholder', 'true');
+      }
+      node.addEventListener('focus', focused, false);
+      node.addEventListener('blur', blurred, false);
+    }
+  };
+
+  document.addEventListener('DOMContentLoaded', loaded, false);
+
+}).call(this);
